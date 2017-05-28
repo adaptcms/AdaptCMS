ab8f3084f80ba7fe31c1141b5e1fe02e6ad75a32
Charlie Page <charliepage@charlies-mbp.attlocal.net>
2017-05-28 15:35:43 -0400

    learn link

--- resources/views/partials/admin-menu-top.blade.php
+++ resources/views/partials/admin-menu-top.blade.php
@@ -5,50 +5,6 @@
     <i class="ui circular blue left label">{{ Cache::get('cms_updates') }}</i>
   @endif
 </a>
-<a class="browse item activate popup">
-  <span>Support</span>
-  <i class="dropdown icon"></i>
-</a>
-<div class="ui fluid popup bottom left transition hidden">
-  <div class="ui four column relaxed equal height divided grid">
-    <div class="column">
-      <h4 class="ui header">Fabrics</h4>
-      <div class="ui link list">
-        <a class="item">Cashmere</a>
-        <a class="item">Linen</a>
-        <a class="item">Cotton</a>
-        <a class="item">Viscose</a>
-      </div>
-    </div>
-    <div class="column">
-      <h4 class="ui header">Size</h4>
-      <div class="ui link list">
-        <a class="item">Small</a>
-        <a class="item">Medium</a>
-        <a class="item">Large</a>
-        <a class="item">Plus Sizes</a>
-      </div>
-    </div>
-    <div class="column">
-      <h4 class="ui header">Colored</h4>
-      <div class="ui link list">
-        <a class="item">Neutrals</a>
-        <a class="item">Brights</a>
-        <a class="item">Pastels</a>
-      </div>
-    </div>
-    <div class="column">
-      <h4 class="ui header">Types</h4>
-      <div class="ui link list">
-        <a class="item">Knitwear</a>
-        <a class="item">Outerwear</a>
-        <a class="item">Pants</a>
-        <a class="item">Shoes</a>
-      </div>
-    </div>
-  </div>
-</div>
-
 <a href="{{ route('admin.updates.browse') }}" class="item">
   <span>Marketplace</span>
   &nbsp;&nbsp;<i class="shopping basket icon"></i>
@@ -57,12 +13,16 @@
   <span>Community</span>
   &nbsp;&nbsp;<i class="users icon"></i>
 </a>
+<a href="https://learn.adaptcms.com" target="_blank" class="item">
+<span>Learn</span>
+&nbsp;&nbsp;<i class="book icon"></i>
+</a>
 <a href="{{ route('logout') }}" class="item">
   <span>Logout</span>
   &nbsp;&nbsp;<i class="sign out icon"></i>
 </a>
 <a href="{{ route('home') }}" class="item">
-  <span>Home Page</span>
+  <span>Public Site</span>
   &nbsp;<i class="home icon"></i>
 </a>
 <div class="right menu">
